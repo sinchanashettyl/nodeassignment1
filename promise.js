aa5ed7d80b65e7942910afc239258f069ee78ae5
sinchanashettyl <113965278+sinchanashettyl@users.noreply.github.com>
2022-10-27 10:06:29 +0530

    Update promise.js

--- promise.js
+++ promise.js
@@ -1,5 +1,10 @@
+//to create the promise and to display a message and call the promise
+
+//created a promise using promise constructor to make an asynchronous call either resolve or reject
 let p = new Promise((resolve, reject) => {
+    //defining variable a
     let a = 1 + 1;
+    //using if condition here the output is successfull promise, the call to resolve moves the promise object to resolved state.
     if (a == 2) {
       resolve("Success");
     } else {
@@ -7,6 +12,7 @@ let p = new Promise((resolve, reject) => {
     }
   });
   
+//as the promise is successfull , the then method returns the promise and goes through resolved state only.
   p.then((message) => {
     console.log(`This is  ${message}`);
   }).catch((message) => {
