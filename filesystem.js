ba7ce09cc25e343efdf2f712d082c93d29af1e06
sinchanashettyl <113965278+sinchanashettyl@users.noreply.github.com>
2022-10-27 10:48:34 +0530

    Update filesystem.js

--- filesystem.js
+++ filesystem.js
@@ -1,8 +1,10 @@
+//create a file using fs module
 const fs = require("fs");
 
-
+//using async function it makes the function return the  promise
 async function writeFile(filename, txt) {
   try {
+    //await function it pause the execution and waits for resolved promise
     await fs.promises.writeFile(filename, txt);
     console.log("File Successfully created");
   } catch (err) {
